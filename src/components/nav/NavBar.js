73acbc2bc2d46082f4c88e9547a6751a2817949a
Yumps <loganpalmer199721@hotmail.com>
2019-05-29 10:01:44 -0500

    Added News components delete isn't currently work as intended, navbar fixed, task component started

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -1,32 +1,37 @@
-import React from 'react';
-import { Navbar, NavItem, NavLink, Nav } from 'reactstrap'
+import React from "react";
+import { Navbar, NavItem, NavLink, Nav } from "reactstrap";
 
 class NavBar extends React.Component {
-
-    render() {
-        return (
-            <div>
-                <Navbar color="faded" light expand="md">
-
-                    <Nav className="ml-auto" navbar>
-                        <NavItem>
-                            <NavLink href="https://github.com/reactstrap/reactstrap">stuff</NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink href="https://github.com/reactstrap/reactstrap">stuff</NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink href="https://github.com/reactstrap/reactstrap">stuff</NavLink>
-                        </NavItem>
-
-                    </Nav>
-                </Navbar>
-            </div>
-
-
-
-        );
-    }
+  render() {
+    return (
+      <div>
+        <Navbar color="dark" faded expand="md">
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink href="/news">
+                <h4>News</h4>
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/events">
+                <h4>Events</h4>
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/tasks">
+                <h4>Tasks</h4>
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/friends">
+                <h4>Friends</h4>
+              </NavLink>
+            </NavItem>
+          </Nav>
+        </Navbar>
+      </div>
+    );
+  }
 }
 
 export default NavBar;
---
+++ src/modules/NewsManager.js
@@ -0,0 +1,30 @@
+const src = "http://localhost:5002";
+
+export default {
+  getAll() {
+    return fetch(`${src}/news`).then(e => e.json());
+  },
+  delete(id) {
+    return fetch(`${src}/news/${id}`, {
+      method: "DELETE"
+    }).then(e => e.json());
+  },
+  post(news) {
+    return fetch(`${src}/news`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(news)
+    }).then(e => e.json());
+  },
+  edit(editedNews) {
+    return fetch(`${src}/news/${editedNews.id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(editedNews)
+    }).then(e => e.json());
+  }
+};
