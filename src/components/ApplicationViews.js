87dd0441e7bee0fa18dbe291c8ca0c43c9af05b9
Hughes Speer <ImHughes@users.noreply.github.com>
2019-05-30 13:16:41 -0500

    app-nav-warning-fixed

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,4 +1,4 @@
-import { Route, Redirect } from 'react-router-dom'
+import { Route, /*Redirect*/ } from 'react-router-dom'
 import { withRouter } from 'react-router'
 import React, { Component } from "react"
 import EventManager from '../modules/EventManager';
